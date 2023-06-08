3d8bab1d62a8fc99e7ffec39f82e92a02d90010c
carlosmoody <otralongo@icloud.com>
2023-06-08 12:07:27 +0200

    ajout des dropdowns

--- src/routes/about/about.jsx
+++ src/routes/about/about.jsx
@@ -1,11 +1,15 @@
 import aboutPicture from "../../assets/kalen-emsley-Bkci_8qcdvQ-unsplash 2.png";
 import styles from "./about.module.css";
 import Banner from "../../components/Banner/Banner";
+import DropdownLarge from "../../components/DropdownLarge/DropdownLarge";
 
 export default function About() {
   return (
     <div className={styles.aboutWrapper}>
       <Banner picture={aboutPicture} altText="Montages" title="" />
+      <div className={styles.aboutContentWrapper}>
+        <DropdownLarge title="Respect" content="Coucou" />
+      </div>
     </div>
   );
 }
