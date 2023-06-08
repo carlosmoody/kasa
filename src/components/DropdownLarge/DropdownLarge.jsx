f9fe59b889661580c5ee662a0b686e5e330bc1c7
carlosmoody <otralongo@icloud.com>
2023-06-08 12:06:55 +0200

    création du dropdown large

---
+++ src/components/DropdownLarge/DropdownLarge.jsx
@@ -0,0 +1,33 @@
+import styles from "./DropdownLarge.module.css";
+import dropdownArrow from "../../assets/dropdownArrow.png";
+import { useState } from "react";
+
+export default function DropdownLarge(props) {
+  const [active, setActive] = useState(false);
+
+  const toggleDropdown = () => {
+    setActive(!active);
+  };
+
+  return (
+    <div className={styles.dropdownWrapper}>
+      <button onClick={toggleDropdown}>
+        <h3>{props.title}</h3>
+        <img
+          src={dropdownArrow}
+          alt="flêche"
+          className={active ? `${styles.icon} ${styles.rotate}` : styles.icon}
+        />
+      </button>
+      <div
+        className={
+          active
+            ? `${styles.answerWrapper} ${styles.showAnswer}`
+            : styles.answerWrapper
+        }
+      >
+        <p>{props.content}</p>
+      </div>
+    </div>
+  );
+}
