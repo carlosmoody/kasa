e911e4d927f53f4525bf8627cc7077dd7596b2ee
carlosmoody <otralongo@icloud.com>
2023-06-08 12:15:29 +0200

    intégration des données a propos

--- src/routes/about/about.jsx
+++ src/routes/about/about.jsx
@@ -2,13 +2,20 @@ import aboutPicture from "../../assets/kalen-emsley-Bkci_8qcdvQ-unsplash 2.png";
 import styles from "./about.module.css";
 import Banner from "../../components/Banner/Banner";
 import DropdownLarge from "../../components/DropdownLarge/DropdownLarge";
+import aboutData from "../../assets/about.json";
 
 export default function About() {
   return (
     <div className={styles.aboutWrapper}>
       <Banner picture={aboutPicture} altText="Montages" title="" />
       <div className={styles.aboutContentWrapper}>
-        <DropdownLarge title="Respect" content="Coucou" />
+        {aboutData.map((question, index) => (
+          <DropdownLarge
+            key={index}
+            title={question.title}
+            content={question.content}
+          />
+        ))}
       </div>
     </div>
   );
