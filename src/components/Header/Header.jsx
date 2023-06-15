f2770b83455a36c7d3f12a4c41bf997071184c53
carlosmoody <otralongo@icloud.com>
2023-06-15 10:42:24 +0200

    remove console log

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,6 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
   const location = useLocation();
-  console.log(location.pathname);
 
   return (
     <div className={styles.headerWrapper}>
